286db76f7c9533f789e52398b2f5036ecaafb7d2
imeshramanayaka@gmail.com <imeshramanayaka988@gmail.com>
2025-05-20 09:33:31 +0530

    initialize project

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    //DOM element load 
+    const story = document.getElementById('stories-grid');
+    const storyView = document.getElementById('story-view');
+    const backButton = document.getElementById('back-button');
+    const storyTittle = document.getElementById('story-title');
+    const storyContent = document.getElementById('story-content');
+    const newParagraphInput = document.getElementById('new-paragraph');
+    const submitParagraphBtn = document.getElementById('new-story-button');
+    const newStoryModel = document.getElementById('new-story-modal');
+    const closeModelBtn = document.getElementById('close-modal');
+    const createStoryBtn = document.getElementById('create-story');
+    const newStoryTittleStoryInput = document.getElementById('new-story-title');
+    const newStoryFirstParagraphInput = document.getElementById('new-story-first-paragraph');
+
+});
---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBGfT78cNZ_tcyfWHcJjoo4ulN-M72ifGc",
+  authDomain: "storyweave-375e2.firebaseapp.com",
+  databaseURL: "https://storyweave-375e2-default-rtdb.firebaseio.com",
+  projectId: "storyweave-375e2",
+  storageBucket: "storyweave-375e2.firebasestorage.app",
+  messagingSenderId: "212006197996",
+  appId: "1:212006197996:web:7e30fd7e991b3c58b963c9",
+  measurementId: "G-LY9DZ086SW"
+};
+
+firebaseConfig.initializeApp(firebaseConfig);
+
+const database = firebaseConfig.database();
+
